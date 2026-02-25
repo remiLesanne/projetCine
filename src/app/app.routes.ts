b650f230d798543f64c9c71c0a121dbbe037e4de
Nassim <nassim.jazouli@epfedu.fr>
2026-02-25 14:58:29 +0100

    feat : ajout de la route inscription (/inscription)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {MoviesList} from './movies-list/movies-list';
 import {Admin} from './admin/admin';
 import {AddMovie} from './add-movie/add-movie';
 import {EditMovie} from './edit-movie/edit-movie';
+import { Inscription } from './user/inscription/inscription';
 
 export const routes: Routes = [
   { path: '', component: Home},
   { path: 'movies', component: MoviesList},
   { path: 'admin', component: Admin},
   { path: 'AddMovie', component: AddMovie},
-  { path: 'edit-movie/:id', component: EditMovie}
+  { path: 'edit-movie/:id', component: EditMovie},
+  { path: 'inscription', component: Inscription}
 ];
