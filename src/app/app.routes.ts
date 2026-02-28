721b362a28eb17399a047c1aa3b2dd851b2d4f22
Gabriel <gabrielcombe04@gmail.com>
2026-02-28 11:07:17 +0100

    FEAT-Ajout Compte Utilisateur

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,11 +4,13 @@ import {MoviesList} from './movies-list/movies-list';
 import {Admin} from './admin/admin';
 import {AddMovie} from './add-movie/add-movie';
 import {EditMovie} from './edit-movie/edit-movie';
+import {AccountUser} from './account-user/account-user';
 
 export const routes: Routes = [
   { path: '', component: Home},
   { path: 'movies', component: MoviesList},
   { path: 'admin', component: Admin},
   { path: 'AddMovie', component: AddMovie},
-  { path: 'edit-movie/:id', component: EditMovie}
+  { path: 'edit-movie/:id', component: EditMovie},
+  { path: 'account/:id', component: AccountUser}
 ];
---
+++ src/app/services/users-api.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+
+@Injectable({
+  providedIn: 'root'  
+})
+export class UsersApi {
+  private readonly http = inject(HttpClient);
+  private readonly apiUrl = 'http://localhost:8080';
+
+  getUsers(): Observable<User[]> {
+    return this.http.get<User[]>(`${this.apiUrl}/users`);
+  }
+
+  getUserById(id: number): Observable<User> {
+    return this.http.get<User>(`${this.apiUrl}/users/${id}`);
+  }
+
+  updateUser(id: number, user: User): Observable<User> {
+    return this.http.put<User>(`${this.apiUrl}/users/${id}`, user);
+  }
+}
